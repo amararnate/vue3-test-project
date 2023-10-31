038d4263d7516512ffdf1ab5cd1abb449ba5f2ce
Amar Parnate <amar.parnate@t-systems.com>
2023-10-31 23:43:19 +0530

    update disabled button color

--- src/components/contextModule/addContext.vue
+++ src/components/contextModule/addContext.vue
@@ -1,46 +1,46 @@
 <script lang="ts" setup>
-import { ref, type Ref } from 'vue'
-import contextTable from './contextTable.vue'
-import addContextModal from './addContextModal.vue'
+import { ref, type Ref } from "vue";
+import contextTable from "./contextTable.vue";
+import addContextModal from "./addContextModal.vue";
 import type {
   ContextData,
   CustomElement,
   DeleteContexRecordMetaData,
-  DeleteContexRecord
-} from '../../components/contextModule/type'
-import ContextDataService from '../../services/ContexDataService'
+  DeleteContexRecord,
+} from "../../components/contextModule/type";
+import ContextDataService from "../../services/ContexDataService";
 
-const disabledDeleteButton = ref(true)
-const addContextModalRef = ref()
-let tableData: Ref<ContextData[]> = ref([])
-const contextDeleteIds: Ref<DeleteContexRecord[]> = ref([])
+const disabledDeleteButton = ref(true);
+const addContextModalRef = ref();
+let tableData: Ref<ContextData[]> = ref([]);
+const contextDeleteIds: Ref<DeleteContexRecord[]> = ref([]);
 
 const addContext = () => {
-  openModal()
-}
+  openModal();
+};
 
 const openModal = () => {
-  const modal = document.getElementById('add-context') as CustomElement
-  modal.opened = true
-  addContextModalRef.value?.resetForm()
-}
+  const modal = document.getElementById("add-context") as CustomElement;
+  modal.opened = true;
+  addContextModalRef.value?.resetForm();
+};
 
 const enableDeleteRecord = (data: DeleteContexRecordMetaData) => {
-  disabledDeleteButton.value = data.showDelete
-  contextDeleteIds.value = data.deleteContexRecordData
-}
+  disabledDeleteButton.value = data.showDelete;
+  contextDeleteIds.value = data.deleteContexRecordData;
+};
 
 const deleteContextRecords = () => {
-  ContextDataService.deleteContexData(contextDeleteIds.value)
-  loadContextData()
-  disabledDeleteButton.value = true
-  contextDeleteIds.value = []
-}
+  ContextDataService.deleteContexData(contextDeleteIds.value);
+  loadContextData();
+  disabledDeleteButton.value = true;
+  contextDeleteIds.value = [];
+};
 
 const loadContextData = () => {
-  tableData.value = ContextDataService.getContexData()
-}
-loadContextData()
+  tableData.value = ContextDataService.getContexData();
+};
+loadContextData();
 </script>
 
 <template>
@@ -51,15 +51,25 @@ loadContextData()
           <scale-button @click="addContext">Add Context</scale-button>
         </div>
         <div class="context-action__delete action-button">
-          <scale-button icon-only :disabled="disabledDeleteButton" @click="deleteContextRecords">
+          <scale-button
+            icon-only
+            :disabled="disabledDeleteButton"
+            @click="deleteContextRecords"
+          >
             <scale-icon-action-remove accessibility-title="remove" selected />
           </scale-button>
         </div>
       </div>
 
-      <context-table :table-data="tableData" @enable-delete-record="enableDeleteRecord" />
+      <context-table
+        :table-data="tableData"
+        @enable-delete-record="enableDeleteRecord"
+      />
 
-      <add-context-modal ref="addContextModalRef" @refresh-list="loadContextData" />
+      <add-context-modal
+        ref="addContextModalRef"
+        @refresh-list="loadContextData"
+      />
     </div>
   </div>
 </template>
@@ -69,6 +79,13 @@ loadContextData()
   display: flex;
   .action-button {
     padding: 10px;
+  }
+
+  &__delete {
+    scale-button::part(disabled) {
+    background: no-repeat;
+    border: 1px solid var(--scl-color-grey-10);
+    }
   }
 }
 </style>
